9288e8bd2d9fb83d189900569149c6b7e98b27ce
Andrew Boylan <andreweboylan@gmail.com>
2026-08-02 15:38:21 -0400

    Expand about feedback link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,9 @@ export default function AboutPage() {
           <p>HTML Music is a place to share recordings and discover music.</p>
         </fieldset>
         <p className="about-bug-report">
-          <a href="mailto:andreweboylan@gmail.com">report a bug</a>
+          <a href="mailto:andreweboylan@gmail.com">
+            report a bug / suggest a feature
+          </a>
         </p>
       </section>
     </main>
